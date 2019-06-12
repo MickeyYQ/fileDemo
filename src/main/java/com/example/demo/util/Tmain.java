cecf4075a8e022c59ef2c5f9f6ff9dadff904b3e
MickeyYQ <www.mickey_qiangzi@163.com>
2019-06-12 21:34:40 +0800

    main commit

---
+++ src/main/java/com/example/demo/util/Tmain.java
@@ -0,0 +1,9 @@
+package com.example.demo.util;
+
+public class Tmain {
+    public static void main(String[] args) {
+        FileScanner fs = new FileScanner();
+        String f = "C:\\Users\\Mickey\\Documents\\2019test.txt";
+        fs.getFile(f);
+    }
+}
